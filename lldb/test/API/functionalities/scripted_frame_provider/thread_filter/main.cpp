7d24e0739a5089b22cf2b49e4a56cd7421b600ef
Med Ismail Bennani <ismail@bennani.ma>
2026-04-07 23:19:28 -0700

    [lldb/test] Replace pause() with atomic barrier in thread_filter test
    The TestFrameProviderThreadFilter test was crashing on the ARM Ubuntu
    CI with an assertion in StackFrameList::GetFrameAtIndex ("A valid
    thread has no frames."):
    
    https://lab.llvm.org/buildbot/#/builders/18/builds/25501
    
    On ARM Linux, the debugger's stop/resume cycle can cause pause() to return
    (EINTR from ptrace signals), letting threads exit before the test accesses
    their frames.
    
    Replace pause() with an atomic spin-loop barrier that keeps all
    threads alive in userspace at a known, unwinding-friendly location.
    
    Signed-off-by: Med Ismail Bennani <ismail@bennani.ma>

--- lldb/test/API/functionalities/scripted_frame_provider/thread_filter/main.cpp
+++ lldb/test/API/functionalities/scripted_frame_provider/thread_filter/main.cpp
@@ -1,11 +1,15 @@
+#include <atomic>
 #include <thread>
-#include <unistd.h>
 #include <vector>
 
 #define NUM_THREADS 3
 
+std::atomic<int> g_barrier(NUM_THREADS);
+
 void thread_work() {
-  pause(); // break in thread
+  --g_barrier;
+  while (g_barrier.load() > 0) // break in thread
+    ;
 }
 
 int main() {
