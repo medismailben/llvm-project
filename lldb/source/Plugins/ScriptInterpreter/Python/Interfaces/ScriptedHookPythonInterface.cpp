4a39a1a662a6710a9e8401453f99522aa0f397a1
Med Ismail Bennani <ismail@bennani.ma>
2026-07-13 20:22:44 -0700

    [lldb] Expand ScriptedExtension coverage across scripted Python interfaces (NFC) (#209307)

--- lldb/source/Plugins/ScriptInterpreter/Python/Interfaces/ScriptedHookPythonInterface.cpp
+++ lldb/source/Plugins/ScriptInterpreter/Python/Interfaces/ScriptedHookPythonInterface.cpp
@@ -97,7 +97,8 @@ void ScriptedHookPythonInterface::Initialize() {
       GetPluginNameStatic(),
       llvm::StringRef("Perform actions on target lifecycle events (module "
                       "load/unload, process stop)."),
-      CreateInstance, eScriptLanguagePython, {ci_usages, api_usages});
+      CreateInstance, eScriptedExtensionScriptedHook, eScriptLanguagePython,
+      {ci_usages, api_usages});
 }
 
 void ScriptedHookPythonInterface::Terminate() {
